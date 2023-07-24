dd5b6d3afcf238842ec5b8494e603cc2c831a5cc
TheBlackParrot <theblackparrot@rocketmail.com>
2023-07-24 18:43:01 -0400

    dropped dependency on spotify-url-info, scraping it myself now

--- linux_stuff/spotify-overlay/main.js
+++ linux_stuff/spotify-overlay/main.js
@@ -1,9 +1,9 @@
 const settings = require("./settings.json");
+const URLVerificationString = "https://open.spotify.com/track/";
 
 const spawn = require('child_process').spawn;
-
-const fetch = require('isomorphic-unfetch'); // i don't know why this was needed, but whatever
-const { getData, getPreview, getTracks, getDetails } = require('spotify-url-info')(fetch)
+const fetch = require('fetch').fetchUrl;
+const cheerio = require('cheerio');
 
 const WebSocketServer = require('ws');
 const wss = new WebSocketServer.Server(settings.listen);
@@ -24,47 +24,85 @@ var playerctl = spawn('playerctl', ['-p', 'spotify', 'metadata', '"xesam:url"', 
 	shell: true
 });
 playerctl.stdout.on('data', function(data) {
-	getData(data).then(function(response) {
-		currentlyPlaying = {
-			title: response.title,
-			artists: [],
-			art: "",
-			colors: {
-				dark: response.coverArt.extractedColors.colorDark.hex,
-				light: response.coverArt.extractedColors.colorLight.hex,
-			},
-			duration: Math.floor(response.duration/1000),
-			startedAt: Date.now(),
-			id: response.id,
-			scannable: {
-				color: {
-					light: `https://scannables.scdn.co/uri/plain/jpeg/${response.coverArt.extractedColors.colorLight.hex.replace("#", "")}/black/640/${response.uri}`,
-					dark: `https://scannables.scdn.co/uri/plain/jpeg/${response.coverArt.extractedColors.colorDark.hex.replace("#", "")}/white/640/${response.uri}`,
-				},
-				monochrome: {
-					light: `https://scannables.scdn.co/uri/plain/jpeg/FFFFFF/black/640/${response.uri}`,
-					dark: `https://scannables.scdn.co/uri/plain/jpeg/000000/white/640/${response.uri}`
+	spotifyURL = data.toString().trim();
+	console.log(spotifyURL);
+
+	if(spotifyURL.substring(0, 31) !== URLVerificationString) {
+		console.log("URL doesn't appear to be correct, ignoring");
+		return;
+	}
+
+	console.log("getting track data...");
+	fetch(spotifyURL, function(err, meta, rawBody) {
+		console.log("got track data");
+		
+		let $ = cheerio.load(rawBody.toString());
+		let rawScriptData = $("#initial-state").text();
+
+		let scriptData = new Buffer.from(rawScriptData, "base64").toString("utf8");
+		let rawJSONData = JSON.parse(scriptData);
+
+		let rawSongData;
+		if("entities" in rawJSONData) {
+			if("items" in rawJSONData.entities) {
+				let keys = Object.keys(rawJSONData.entities.items);
+				if(keys.length) {
+					rawSongData = rawJSONData.entities.items[keys[0]];
 				}
 			}
 		}
 
-		for(let i in response.artists) {
-			let artist = response.artists[i];
-			currentlyPlaying.artists.push(artist.name);
+		if(!rawSongData) {
+			return;
+		}
+
+		currentlyPlaying = {
+			title: rawSongData.name,
+			artists: [],
+			art: "",
+			colors: {
+				dark: rawSongData.albumOfTrack.coverArt.extractedColors.colorDark.hex,
+				light: rawSongData.albumOfTrack.coverArt.extractedColors.colorDark.hex,
+			},
+			duration: rawSongData.duration.totalMilliseconds/1000,
+			startedAt: Date.now(),
+			id: rawSongData.id,
+			scannable: {
+				color: {
+					light: `https://scannables.scdn.co/uri/plain/jpeg/${rawSongData.albumOfTrack.coverArt.extractedColors.colorDark.hex.replace("#", "")}/black/640/${rawSongData.uri}`,
+					dark: `https://scannables.scdn.co/uri/plain/jpeg/${rawSongData.albumOfTrack.coverArt.extractedColors.colorDark.hex.replace("#", "")}/white/640/${rawSongData.uri}`,
+				},
+				monochrome: {
+					light: `https://scannables.scdn.co/uri/plain/jpeg/FFFFFF/black/640/${rawSongData.uri}`,
+					dark: `https://scannables.scdn.co/uri/plain/jpeg/000000/white/640/${rawSongData.uri}`
+				}
+			}
+		}
+
+		for(let i in rawSongData.firstArtist.items) {
+			let artist = rawSongData.firstArtist.items[i];
+			currentlyPlaying.artists.push(artist.profile.name);
+		}
+		if("otherArtists" in rawSongData) {
+			for(let i in rawSongData.otherArtists.items) {
+				let artist = rawSongData.otherArtists.items[i];
+				currentlyPlaying.artists.push(artist.profile.name);
+			}
 		}
 
 		let oldMax = 0;
-		for(let i in response.coverArt.sources) {
-			let src = response.coverArt.sources[i];
+		for(let i in rawSongData.albumOfTrack.coverArt.sources) {
+			let src = rawSongData.albumOfTrack.coverArt.sources[i];
 			if(src.width > oldMax) {
 				currentlyPlaying.art = src.url;
 				oldMax = src.width;
 			}
 		}
 
+		console.log(currentlyPlaying);
 		sendData(currentlyPlaying);
-	})
-})
+	});
+});
 
 function sendData(data) {
 	for(let wsIdx in wsClients) {
